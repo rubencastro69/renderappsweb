b56b1ee041a4a541a35c7ba7fadb82fac33d0100
Rubén Alonso Castro Molina <ruben0325@outlook.com>
2025-11-11 17:10:31 -0700

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,11 +2,17 @@ import express from 'express';
 import {
     createPost,
     getAllPosts,
+    getPostById,
+    updatePost,
+    deletePost
 } from '../controllers/postController.js'
 
 const router = express.Router();
 
 router.post('/', createPost);
 router.get('/', getAllPosts);
+router.get('/:id', getPostById);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
